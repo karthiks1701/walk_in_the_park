98c491c24a9a0e473b606053e6e58ce146b48f61
karthiks1701 <soma.nitt@gmail.com>
2025-06-15 20:30:58 -0400

    jax stuff for docker

--- rl/networks/common.py
+++ rl/networks/common.py
@@ -8,6 +8,8 @@ default_init = nn.initializers.xavier_uniform
 def soft_target_update(
     critic_params: FrozenDict, target_critic_params: FrozenDict, tau: float
 ) -> FrozenDict:
+    # new_target_params = jax.tree_map(lambda p, tp: p * tau + tp * (1 - tau),
+    #                                  critic_params, target_critic_params)
     new_target_params = jax.tree_util.tree_map(
         lambda p, tp: p * tau + tp * (1 - tau), critic_params, target_critic_params
     )
