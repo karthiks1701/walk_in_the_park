a0ee66a492925c59a32e6c6cd58626f3d4a6e597
karthiks1701 <soma.nitt@gmail.com>
2025-06-15 20:29:37 -0400

    fix some jax stuff

--- rl/networks/common.py
+++ rl/networks/common.py
@@ -5,10 +5,11 @@ from flax.core import FrozenDict
 default_init = nn.initializers.xavier_uniform
 
 
-def soft_target_update(critic_params: FrozenDict,
-                       target_critic_params: FrozenDict,
-                       tau: float) -> FrozenDict:
-    new_target_params = jax.tree_map(lambda p, tp: p * tau + tp * (1 - tau),
-                                     critic_params, target_critic_params)
+def soft_target_update(
+    critic_params: FrozenDict, target_critic_params: FrozenDict, tau: float
+) -> FrozenDict:
+    new_target_params = jax.tree_util.tree_map(
+        lambda p, tp: p * tau + tp * (1 - tau), critic_params, target_critic_params
+    )
 
     return new_target_params
